8ef7de9f38cddfb9b39bd125b319202cec1d3767
Joush <37282701+RexJoush@users.noreply.github.com>
2021-04-21 15:28:57 +0800

    init 2021.04.21

--- src/util/LocationUtils.java
+++ src/util/LocationUtils.java
@@ -13,20 +13,20 @@ public class LocationUtils {
     private static final double EARTH_RADIUS = 6378.137;
 
     public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
-        // 纬度
+        // latitude
         double lat1 = Math.toRadians(latitude1);
         double lat2 = Math.toRadians(latitude2);
-        // 经度
+        // longitude
         double lng1 = Math.toRadians(longitude1);
         double lng2 = Math.toRadians(longitude2);
-        // 纬度之差
+        // difference of latitude
         double a = lat1 - lat2;
-        // 经度之差
+        // difference of longitude
         double b = lng1 - lng2;
-        // 计算两点距离的公式
+        // calculate distance between two point
         double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
                 Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(b / 2), 2)));
-        // 弧长乘地球半径, 返回单位: 千米
+        // radians * earth radius, return kilometer
         s =  s * EARTH_RADIUS;
         return s;
     }
